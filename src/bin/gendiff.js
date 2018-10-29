b982e10e9a22d86a47feda5d983efb38d70e59f9
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-10-29 22:02:54 +0200

    add read files

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,13 +4,29 @@
  * Module dependencies.
  */
 
-import { version } from '../../package.json';
+import fs from 'fs';
+import path from 'path';
+import program from 'commander';
+import version from '../../package.json';
 
-const program = require('commander');
+const genDiff = (pathToFile1, pathToFile2) => {
+  const file1 = fs.readFileSync(pathToFile1, 'utf-8');
+  const file2 = fs.readFileSync(pathToFile2, 'utf-8');
+
+  console.log(file1);
+  console.log(file2);
+
+  return 'hello';
+};
+
 
 program
   .version(version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<firstConfig> <secondConfig>')
-  .option('-f, --format [type]', 'Output format')
-  .parse(process.argv);
+  .action((firstConfig, secondConfig) => genDiff(firstConfig, secondConfig))
+  .option('-f, --format [type]', 'Output format');
+
+program.parse(process.argv);
+
+export default genDiff;
