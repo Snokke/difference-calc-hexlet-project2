1f3c5ea5865efc899543d2cdc31344ecadc47ccd
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-10-29 13:59:48 +0200

    add help

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
 
-console.log('Project 2');
+/**
+ * Module dependencies.
+ */
+
+const program = require('commander');
+
+program
+  .version('0.1.3')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<firstConfig> <secondConfig>')
+  .option('-f, --format [type]', 'Output format')
+  .parse(process.argv);
