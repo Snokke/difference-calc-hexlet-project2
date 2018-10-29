c4f750d2b3585bda98682e14df54963197fb338d
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-10-30 01:44:29 +0200

    update tests

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 import path from 'path';
-import genDiff from '../bin/gendiff';
+import genDiff from '..';
 
 test('json diff', () => {
   const expectedFixturePath = path.join(__dirname, '__fixtures__', 'jsondiff');
   const expected = fs.readFileSync(expectedFixturePath, 'utf-8');
-  expect(genDiff('../../before.json', '../../after.json')).toBe(expected);
+  expect(genDiff('before.json', 'after.json')).toBe(expected);
 });
