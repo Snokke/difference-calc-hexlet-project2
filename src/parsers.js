77e9651acb4ab29fc7542a04adea55e986b259b6
Andrei Babintsev <snakemgl.ukraine@gmail.com>
2018-10-31 19:39:29 +0200

    fix object mappingExtensions

--- src/parsers.js
+++ src/parsers.js
@@ -2,9 +2,9 @@ import yaml from 'js-yaml';
 import ini from 'ini';
 
 const mappingExtensions = {
-  json: file => JSON.parse(file),
-  yaml: file => yaml.safeLoad(file),
-  ini: file => ini.parse(file),
+  json: JSON.parse,
+  yaml: yaml.safeLoad,
+  ini: ini.parse,
 };
 
 const parse = (data, extension) => mappingExtensions[extension](data);
